58b1fe1f351a2b3077dc2df815a4551908759560
Trevor hoffman <trevorhoffman@Trevors-MacBook-Air.local>
2019-07-09 11:36:54 -0600

    server is running

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
 var express = require("express");
 var express_graphql = require("express-graphql");
 var { buildSchema } = require("graphql");
-
-// i am not sure why this is not working and why it wont pull my repos
