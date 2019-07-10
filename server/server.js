49efff5b5c66b4842a079608e9c67d69f8c7310a
Trevor hoffman <trevorhoffman@trevors-air.home>
2019-07-10 07:36:36 -0500

    graphiql is now accessible

--- server/server.js
+++ server/server.js
@@ -1,3 +1,28 @@
 var express = require("express");
 var express_graphql = require("express-graphql");
 var { buildSchema } = require("graphql");
+
+// GraphQL schema
+var schmea = buildSchema(`
+type Query {
+    message: String
+}
+`);
+var root = {
+  message: () => "Hello World"
+};
+
+//Express server and Graphql endpoint
+var app = express();
+app.use(
+  "/graphql",
+  express_graphql({
+    schema: schmea,
+    rootValue: root,
+    graphiql: true
+  })
+);
+
+app.listen(4000, () => {
+  console.log(`Express Graphql server running here again`);
+});
